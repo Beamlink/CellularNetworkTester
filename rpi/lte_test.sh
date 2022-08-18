17a464b0e00e070353e3af491d80f324e74f0632
Arpad Kovesdy <arpad@beamlink.io>
2022-08-18 22:13:23 +0000

    added lte_test.sh

---
+++ rpi/lte_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#This is a startup script to connect sim7600x with Raspberry  pi
+#LTE Connection is established 
+
+sudo ifconfig wwan0 mtu 1430 up   #Temporaryly changes the  MTU of wwan0 from default 1500 to 1430 
+
+sudo qmicli -d /dev/cdc-wdm1 --dms-set-operating-mode='online' 
+
+sudo qmicli -d /dev/cdc-wdm1 --dms-get-operating-mode 
+
+sudo qmicli -d /dev/cdc-wdm1 --nas-get-signal-strength 
+
+sudo qmicli -d /dev/cdc-wdm1 --nas-get-home-network 
+
+sudo ip link set wwan0 down
+
+echo 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip
+
+sudo ip link set wwan0 up 
+
+sudo ip link set wlan0 down 
+
+sudo qmicli -p -d /dev/cdc-wdm1 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="ip-type=4" --client-no-release-cid 
+
+sudo udhcpc -i wwan0
+
+ip a s wwan0 
