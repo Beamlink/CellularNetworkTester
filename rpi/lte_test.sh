04f217b0abd76097d3b09ee9b30ca622bb9dcf4c
Arpad Kovesdy <kovesdy@usc.edu>
2022-08-18 17:03:45 -0700

    Fixed issue with Pi Zero and script using qmi_wwan

--- rpi/lte_test.sh
+++ rpi/lte_test.sh
@@ -3,15 +3,21 @@
 #This is a startup script to connect sim7600x with Raspberry  pi
 #LTE Connection is established 
 
+#See: https://raspberrypi.stackexchange.com/questions/68978/loading-kernel-module-qmi-wwan-before-option-to-get-dev-cdc-wdm0-automatica
+#Removes the option driver and then forces the module to utilize the qmi_wwan
+sudo modprobe -r option
+sudo modprobe -r qmi_wwan
+sudo modprobe qmi_wwan
+
 sudo ifconfig wwan0 mtu 1430 up   #Temporaryly changes the  MTU of wwan0 from default 1500 to 1430 
 
-sudo qmicli -d /dev/cdc-wdm1 --dms-set-operating-mode='online' 
+sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online' 
 
-sudo qmicli -d /dev/cdc-wdm1 --dms-get-operating-mode 
+sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode 
 
-sudo qmicli -d /dev/cdc-wdm1 --nas-get-signal-strength 
+sudo qmicli -d /dev/cdc-wdm0 --nas-get-signal-strength 
 
-sudo qmicli -d /dev/cdc-wdm1 --nas-get-home-network 
+sudo qmicli -d /dev/cdc-wdm0 --nas-get-home-network 
 
 sudo ip link set wwan0 down
 
